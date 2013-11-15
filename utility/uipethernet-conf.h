0c83d7c401785ff621a22cb043f44b063b7d21e7
ntruchsess <norbert.truchsess@t-online.de>
2013-11-15 18:21:33 +0100

    enable UDP in uipethernet-conf.h

---
+++ utility/uipethernet-conf.h
@@ -0,0 +1,14 @@
+#ifndef UIPETHERNET_CONF_H
+#define UIPETHERNET_CONF_H
+
+/* for TCP */
+#define UIP_SOCKET_NUMPACKETS    5
+#define UIP_CONF_MAX_CONNECTIONS 4
+
+/* for UDP */
+#define UIP_CONF_UDP             1
+#define UIP_CONF_BROADCAST       1
+#define UIP_CONF_UDP_CONNS       4
+#define UIP_UDP_NUMPACKETS       5
+
+#endif
